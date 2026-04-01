b3b31c3c816a454f2314be815c357d4c5243f727
Biswajit <biswajitkr.dandapat@gmail.com>
2026-04-02 01:24:14 +0530

    db url fix

--- backend/app/database.py
+++ backend/app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-DATABASE_URL = "postgresql://postgres:password@localhost:5432/safesphere"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
