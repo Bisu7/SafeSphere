6845c10a27d9ee70044e773d46854a2521883064
Biswajit <biswajitkr.dandapat@gmail.com>
2026-04-06 22:20:22 +0530

    router fix

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from app.routes import auth, user
+from app.routes import auth, user as user_routes
 from app.models import user, scan, activity
 from app.database import Base, engine
 
@@ -7,5 +7,9 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+@app.get("/")
+def read_root():
+    return {"message": "SafeSphere API is running!"}
+
 app.include_router(auth.router)
-app.include_router(user.router)
+app.include_router(user_routes.router)
