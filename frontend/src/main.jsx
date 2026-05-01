2caef60bd88d21983dab9a1d451a31d92eb9648d
Biswajit <biswajitkr.dandapat@gmail.com>
2026-05-01 15:42:48 +0530

    app.jsx import updated

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
-import ThemeProvider from './components/ThemeContext.jsx' // 1. Import the provider
+import App from './App'
+import ThemeProvider from './components/ThemeContext' // 1. Import the provider
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
